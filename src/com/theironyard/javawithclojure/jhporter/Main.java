369dd389534a2bc6b00e88868d5b2512c9919cd3
jeffryporter <jporter7@gmail.com>
2016-05-23 12:01:52 -0400

    refactored options

--- src/com/theironyard/javawithclojure/jhporter/Main.java
+++ src/com/theironyard/javawithclojure/jhporter/Main.java
@@ -6,58 +6,75 @@ import java.util.Scanner;
 public class Main
 {
 
+    public static void createItem(Scanner input, ArrayList<ToDoItem> items)
+    {
+        System.out.printf("\nPlease enter your to-do item: ");
+        String text = input.nextLine();
+        ToDoItem item = new ToDoItem(text, false);
+        items.add(item);
+    }
+
+    public static void toggleItem(Scanner input, ArrayList<ToDoItem> items)
+    {
+        System.out.printf("\nEnter the number of the item you wish to toggle: ");
+        String numStr = input.nextLine();
+        try
+        {
+            int num = Integer.valueOf(numStr);
+            ToDoItem tempItem = items.get(num - 1);
+            tempItem.setDone(!tempItem.isDone());
+        }
+        catch (NumberFormatException e)
+        {
+            System.err.printf("\nYou Didn't Type A Number!!!");
+        }
+        catch (IndexOutOfBoundsException e)
+        {
+            System.err.printf("\nItem Does Not Exist!!!");
+        }
+    }
+
+    public static void showItems(ArrayList<ToDoItem> items)
+    {
+        int i = 1;
+        for( ToDoItem toDoItem : items)
+        {
+            String checkBox = "[ ]";
+            if(toDoItem.isDone()) {
+                checkBox = "[x]";
+            }
+            System.out.printf("\n%s%d. %s", checkBox, i, toDoItem.getText());
+            i++;
+        }
+        System.out.printf("\n\n");
+    }
+
     public static void main(String[] args)
     {
         ArrayList<ToDoItem> items = new ArrayList<>();
         Scanner input = new Scanner(System.in);
-
-        while (true)
+        boolean run = true;
+        while (run)
         {
-            System.out.printf("\n1. Create Item.\n2. Toggle Item\n3. List Items\n");
+            System.out.printf("\n1. Create Item\n2. Toggle Item\n3. List Items\n4. Quit");
             String option = input.nextLine();
 
             switch(option)
             {
                 case "1":
-                    System.out.printf("\nPlease enter your to-do item: ");
-                    String text = input.nextLine();
-                    ToDoItem item = new ToDoItem(text, false);
-                    items.add(item);
+                    createItem(input, items);
                     break;
                 case "2":
-                    System.out.printf("\nEnter the number of the item you wish to toggle: ");
-                    String numStr = input.nextLine();
-                    try
-                    {
-                        int num = Integer.valueOf(numStr);
-                        ToDoItem tempItem = items.get(num - 1);
-                        tempItem.setDone(!tempItem.isDone());
-                    }
-                    catch (NumberFormatException e)
-                    {
-                        System.err.printf("\nYou Didn't Type A Number!!!");
-                    }
-                    catch (ArrayIndexOutOfBoundsException e)
-                    {
-                        System.err.printf("\nItem Does Not Exist!!!!");
-                    }
-
+                    toggleItem(input, items);
                     break;
                 case "3":
-                    int i = 1;
-                    for( ToDoItem toDoItem : items)
-                    {
-                        String checkBox = "[ ]";
-                        if(toDoItem.isDone()) {
-                            checkBox = "[x]";
-                        }
-                        System.out.printf("\n%s%d. %s", checkBox, i, toDoItem.getText());
-                        i++;
-                    }
-                    System.out.printf("\n\n");
+                    showItems(items);
+                    break;
+                case "4":
+                    run = false;
                     break;
                 default:
-                    System.err.printf("\nInvalid Option!");
+                    System.err.printf("\nInvalid Option!\n");
                     break;
             }
         }
